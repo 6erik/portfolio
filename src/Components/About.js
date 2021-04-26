864cb8e2a29cafd14831546b7fdb488cc310c064
6erik <estanton@csumb.edu>
2021-04-26 01:47:23 -0700

    Added more links to project pages. Adjusted CSS

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,16 +2,16 @@ function About() {
     return (
       <div id="about-content" className="animate-fade">
           <strong>Education: </strong> B.S., Computer Science @ CSU Monterey Bay <br />
-          <strong>GitHub: </strong> <a href="https://github.com/6erik" target="_blank">github.com/6erik</a> <br />
+          <strong>GitHub: </strong> <a href="https://github.com/6erik" target="_blank" rel="noopener noreferrer">github.com/6erik</a> <br />
           <strong>E-Mail: </strong> erikstanton@protonmail.com <br />
           <strong>Resume: </strong>
-            <a href="https://github.com/6erik/portfolio/raw/master/src/files/word_resume.docx" target="_blank">[Word]</a>
-            <a href="https://github.com/6erik/portfolio/raw/master/src/files/pdf_resume.pdf" target="_blank">[PDF]</a><br />
+            <a href="https://github.com/6erik/portfolio/raw/master/src/files/word_resume.docx" target="_blank" rel="noopener noreferrer">[ Word ]</a>&nbsp;&nbsp;
+            <a href="https://github.com/6erik/portfolio/raw/master/src/files/pdf_resume.pdf" target="_blank" rel="noopener noreferrer">[ PDF ]</a><br />
           <strong>Interests: </strong>
           <ul>
             <li>Software and UI development </li>
             <li>Improving algorithm efficiency </li>
-            <li>Analysis and statistics of sports, casino, and video games </li>
+            <li>Analysis of data and statistics in sports, casino, and video games </li>
           </ul>
       </div>
     )
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
 import Carousel from "react-bootstrap/Carousel";
 
 import petrecs1 from '../img/petrecs_corner.png';
@@ -33,7 +32,7 @@ function Projects() {
               <span className="projects-item-content" onClick={handleShow1}>
                 <img src={petrecs1} width="342" height="250" alt="Screenshot of Pet Recs app"/>
                 <strong>PetRecs</strong><br />
-                A web app made with ReactJS, used by pet owners and vets to upload, track, and share pet healthcare documents and data online
+                A web app made with ReactJS, for pet owners and vets to upload, track, and share pet healthcare documents and data online
               </span>
             </Col>
 
@@ -50,7 +49,7 @@ function Projects() {
               <span className="projects-item-content" onClick={handleShow3}>
               <img src={typingtest1} width="342" height="250" alt="Screenshot of Typing Test app"/>
                 <strong>Java Typing Test</strong><br />
-                A web app made with JS, where users can practice typing the common formatting, syntax, and symbols used in Java code
+                A web app made with JavaScript, where users can practice typing the common formatting, syntax, and symbols used in Java code
               </span>
             </Col>
           </Row>
@@ -58,30 +57,38 @@ function Projects() {
 
         <Modal show={show1} onHide={handleClose1}>
           <Modal.Header closeButton>
-            <Modal.Title>PetRecs</Modal.Title>
+            <Modal.Title>
+              PetRecs
+            </Modal.Title>
           </Modal.Header>
 
           <Modal.Body>
             <Carousel>
-              <Carousel.Item>
-                test image 1
-              </Carousel.Item>
-              <Carousel.Item>
-                test image 2
-              </Carousel.Item>
-              <Carousel.Item>
-                test image 3
-              </Carousel.Item>
-            </Carousel>
-            <ul>Tech used:</ul>
-            <li>MySQL</li>
-            <li>Express.js</li>
-            <li>React</li>
-            <li>Node.js</li>
+                <Carousel.Item>
+                  test image 1
+                </Carousel.Item>
+                <Carousel.Item>
+                  test image 2
+                </Carousel.Item>
+                <Carousel.Item>
+                  test image 3
+                </Carousel.Item>
+              </Carousel>
+              <br />
+              <strong>Tech used: </strong>
+              <ul>
+                <li>React</li>
+                <li>Express.js</li>
+                <li>Node.js</li>
+                <li>MySQL</li>
+              </ul>
           </Modal.Body>
 
           <Modal.Footer className="modal-footer">
-            <a href="https://petrecs.herokuapp.com/" target="_blank">[Webpage]</a>
+            <a href="https://petrecs.herokuapp.com/" target="_blank" rel="noopener noreferrer">[ Webpage ]</a> &nbsp;&nbsp;
+            <a href="https://github.com/LeviRemi/PetRecs" target="_blank" rel="noopener noreferrer">[ Repository ]</a> &nbsp;&nbsp;
+            <a href="https://www.youtube-nocookie.com/embed/SjbETH8Fx_k" target="_blank" rel="noopener noreferrer">[ Presentation+Demo ]</a> &nbsp;&nbsp;
+            
           </Modal.Footer>
         </Modal>
 
@@ -92,12 +99,27 @@ function Projects() {
           </Modal.Header>
 
           <Modal.Body>
-            
-            Tech used:
+            <Carousel>
+                <Carousel.Item>
+                  test image 1
+                </Carousel.Item>
+                <Carousel.Item>
+                  test image 2
+                </Carousel.Item>
+                <Carousel.Item>
+                  test image 3
+                </Carousel.Item>
+              </Carousel>
+              <br />
+              <strong>Tech used: </strong>
+              <ul>
+                <li>Python</li>
+                <li>Tkinter</li>
+              </ul>
           </Modal.Body>
 
           <Modal.Footer className="modal-footer">
-            <a href="https://github.com/6erik/craps-logger" target="_blank">[GitHub]</a>
+            <a href="https://github.com/6erik/craps-logger" target="_blank" rel="noopener noreferrer">[Repository]</a>
           </Modal.Footer>
         </Modal>
 
@@ -107,12 +129,29 @@ function Projects() {
           </Modal.Header>
 
           <Modal.Body>
-            Tech used: ReactJS
+            <Carousel>
+                <Carousel.Item>
+                  test image 1
+                </Carousel.Item>
+                <Carousel.Item>
+                  test image 2
+                </Carousel.Item>
+                <Carousel.Item>
+                  test image 3
+                </Carousel.Item>
+              </Carousel>
+              <br />
+              <strong>Tech used: </strong>
+              <ul>
+                <li>JavaScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+              </ul>
           </Modal.Body>
 
           <Modal.Footer className="modal-footer">
-            <a href="https://6erik.github.io/typing-test" target="_blank">[Webpage]</a> &nbsp;
-            <a href="https://github.com/6erik/typing-test" target="_blank">[GitHub]</a>
+            <a href="https://6erik.github.io/typing-test" target="_blank" rel="noopener noreferrer">[Webpage]</a> &nbsp;
+            <a href="https://github.com/6erik/typing-test" target="_blank" rel="noopener noreferrer">[Repository]</a>
           </Modal.Footer>
         </Modal>
       </div>
